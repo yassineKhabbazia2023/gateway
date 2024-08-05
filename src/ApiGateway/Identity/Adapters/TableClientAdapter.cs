bf1c274fa0efd20244eb678fee70811cb50e32ab
Oussema Ben Amor <obenamor@kpmg.fr>
2024-08-05 08:51:02 +0000

    Merged PR 33432: Update code to use managed identity
    Related work items: #331148

--- src/ApiGateway/Identity/Adapters/TableClientAdapter.cs
+++ src/ApiGateway/Identity/Adapters/TableClientAdapter.cs
@@ -1,5 +1,6 @@
 ﻿using Azure;
 using Azure.Data.Tables;
+using Azure.Identity;
 
 namespace ApiGateway.Identity.Adapters
 {
@@ -9,7 +10,7 @@ namespace ApiGateway.Identity.Adapters
 
         public TableClientAdapter(string storageUri, string tableName, TableSharedKeyCredential credentials)
         {
-            this.tableClient = CreateTableClient(storageUri, tableName,credentials);
+            this.tableClient = CreateTableClient(storageUri, tableName);
         }
 
         public  AsyncPageable<TableEntity> Query(string filter)
@@ -17,12 +18,41 @@ namespace ApiGateway.Identity.Adapters
             return this.tableClient.QueryAsync<TableEntity>(filter);
         }
 
-        private TableClient CreateTableClient(string storageUri, string tableName, TableSharedKeyCredential credentials)
+        private TableClient CreateTableClient(string storageUri, string tableName)
         {
-            return new TableClient(
-                new Uri(storageUri),
-                tableName,
-                credentials);
+            var credential = new ManagedIdentityCredential();
+
+            if (!storageUri.EndsWith("/"))
+            {
+                storageUri += "/";
+            }
+           
+            var serviceClient = new TableServiceClient(new Uri(storageUri), credential);
+
+            
+            var tableClient = serviceClient.GetTableClient(tableName);
+
+            return tableClient;
         }
+
+        #region only to test api in local env
+        /**
+         * To test the API locally:
+         * 1. Comment out the CreateTableClient method that uses the managed identity.
+         * 2. Uncomment this method.
+         * 3. Ensure the following environment variables are set:
+         *    - IsvcAzureStorageKey
+         *    - IsvcAzureStorageUri
+         *    - IsvcAzureStorageName
+         **/
+
+        //private TableClient CreateTableClient(string storageUri, string tableName, TableSharedKeyCredential credentials)
+        //{
+        //    return new TableClient(
+        //        new Uri(storageUri),
+        //        tableName,
+        //        credentials);
+        //}
+        #endregion
     }
 }
