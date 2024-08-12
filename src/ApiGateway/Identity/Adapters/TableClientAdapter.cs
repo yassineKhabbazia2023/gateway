c7dc48b03ebf4a1362710e187a654caeb36381ea
Oussema Ben Amor <obenamor@kpmg.fr>
2024-08-12 15:22:10 +0200

    chore: remove commented code

--- src/ApiGateway/Identity/Adapters/TableClientAdapter.cs
+++ src/ApiGateway/Identity/Adapters/TableClientAdapter.cs
@@ -34,25 +34,5 @@ namespace ApiGateway.Identity.Adapters
 
             return tableClient;
         }
-
-        #region only to test api in local env
-        /**
-         * To test the API locally:
-         * 1. Comment out the CreateTableClient method that uses the managed identity.
-         * 2. Uncomment this method.
-         * 3. Ensure the following environment variables are set:
-         *    - IsvcAzureStorageKey
-         *    - IsvcAzureStorageUri
-         *    - IsvcAzureStorageName
-         **/
-
-        //private TableClient CreateTableClient(string storageUri, string tableName, TableSharedKeyCredential credentials)
-        //{
-        //    return new TableClient(
-        //        new Uri(storageUri),
-        //        tableName,
-        //        credentials);
-        //}
-        #endregion
     }
 }
