d1ff7a42e561a43d48f1ee14f736be5328a0cd60
Dibeh, Marc <mdibeh@kpmg.fr>
2024-11-29 15:08:33 +0100

    adding common MiddleWare extensions that can be used for different middleware

---
+++ src/ApiGateway/Extensions/CommonMiddleWareExtensions.cs
@@ -0,0 +1,58 @@
+﻿using ApiGateway.Identity.Extensions;
+using ApiGateway.Identity;
+using Ocelot.Authorization;
+using Ocelot.Middleware;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace ApiGateway.Extensions
+{
+    public static class CommonMiddleWareExtensions
+    {
+
+        public static void ForbiddenRequest(this HttpContext httpContext)
+        {
+            var downstreamRoute = httpContext.Items.DownstreamRoute();
+            httpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
+            httpContext.Items.SetError(new UnauthorizedError(
+                               $"{httpContext!.User!.Identity!.Name} unable to access {downstreamRoute.UpstreamPathTemplate.OriginalValue}"));
+        }
+
+        public static void ForbiddenAccount(this HttpContext httpContext, int accountId)
+        {
+            httpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
+            httpContext.Items.SetError(new UnauthorizedError(
+                               $"{httpContext!.User!.Identity!.Name} unable to access {accountId}"));
+        }
+        public static async Task<bool> IdentityServiceValidations(this HttpContext httpContext, string userEmail, IIdentityService identityServiceProvider)
+        {
+            bool isCollaborator = httpContext.User.IsCollaborator();
+            bool isCustomer = httpContext.User.IsCustomer();
+
+            if (isCollaborator)
+            {
+                var isValidCollaborator = identityServiceProvider.ValidateCollaborator(httpContext);
+                if (!isValidCollaborator)
+                {
+                    ForbiddenRequest(httpContext);
+                    return false;
+                }
+            }
+
+            if (isCustomer)
+            {
+                var isValidCustomer = await identityServiceProvider.ValidateCustomerAsync(userEmail);
+                if (!isValidCustomer)
+                {
+                    ForbiddenRequest(httpContext);
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        
+
+    }
+}
