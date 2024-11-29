85b4d717d8f87419b496403a0a2b3c0064681bc7
Dibeh, Marc <mdibeh@kpmg.fr>
2024-11-29 15:08:01 +0100

    add caching constants

--- src/ApiGateway/Constants/GlobalConstants.cs
+++ src/ApiGateway/Constants/GlobalConstants.cs
@@ -9,6 +9,9 @@ public static class GlobalsConstants
     public static readonly string[] NoAccountCheckEndpoints = ["ged-services", "/api/authorizations/configuration/account"];
     public static readonly int CollaboratorAccountId = -1;
     public static readonly string AccountIdHeader = "Account-Id";
+    public static readonly string cacheAccountId = "cache-accountId";
+    public static readonly string cacheContactId = "cache-contactId";
+    public static readonly string cacheContent = "cache-content";
     public static readonly JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings
     {
         ContractResolver = new CamelCasePropertyNamesContractResolver()
